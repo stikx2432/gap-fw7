83b00c1f65f9f3793fbbc389da2d950a20dd7ce0
stikx2432 <stikx2432@gmail.com>
2016-12-21 18:43:16 -0800

    testing

--- fw7/myFirstFW7app/www/js/my-app.js
+++ fw7/myFirstFW7app/www/js/my-app.js
@@ -5,11 +5,18 @@ Template7.registerHelper('json_stringify', function (context) {
 
 // Init couchDB and do magic
 var db = new PouchDB('test_remote');
-var holderObj;
+
+var localData = [];
+getData()
 db.info().then(function (info) {
   holderObj = info;
   console.log(holderObj);
 })
+
+
+
+
+
 
 // Initialize your app
 var myApp = new Framework7({
@@ -88,15 +95,14 @@ var myApp = new Framework7({
             },
         ],
 
-        // Another plain data object, used in "about" link in data-contextName object 
-        "about": {
-            "name": "John Doe",
-            "age": 32,
-            "position": "CEO",
-            "company": "Google",
-            "interests": ["swimming", "music", "JavaScript", "iMac", "iOS apps", "sport"]
-        }
-    }
+        // Another plain data object, used in "tasks" link in data-contextName object
+        tasks: localData
+    },
+    taskPanel: people = [
+      {
+        firstName: 'John', lastName: 'Doe'
+      }
+    ]
 });
 
 // Export selectors engine
@@ -107,3 +113,75 @@ var mainView = myApp.addView('.view-main', {
     // Enable dynamic Navbar
     dynamicNavbar: true,
 });
+
+var sideView = myApp.addView('.view-panel', {
+
+});
+
+// After Load Views
+
+$$('#taskButton').on('click', function () {
+  var inputText = $$('#task').val();
+  var randId = randomString(6);
+  console.log(randId);
+  if (inputText !== '') {
+      db.get(randId).catch(function(err) {
+          if (err.name === 'not_found') {
+              return {
+                  '_id': randId,
+                  'task': inputText
+              }
+          } else {
+              throw err;
+          }
+      }).then(function (doc) {
+
+          doc._id = randId;
+          doc.task = inputText;
+          // global object to hold tasks for template7Data
+
+          console.log(doc);
+
+          return db.put(doc);
+        }).then(function() {
+            return db.get(randId).then(function(doc) {
+              myApp.alert('Task: ' + doc.task + 'Task _id:' + doc._id + ' inputed to DB.', 'Success!')
+            })
+          })
+    } else {
+      myApp.alert('You didn\'t input anything', 'Hey!', function () {
+
+      })
+    }
+
+
+  });
+
+
+
+function getData () {
+  db.allDocs({
+    include_docs: true,
+    attachments: true
+  }).then(function (result) {
+
+    for (var i = 0; i < result.rows.length; i++) {
+      if (result.rows[i].doc.task !== undefined) {
+        localData.push( { task: result.rows[i].doc.task } );
+      }
+
+    }
+
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+var randomString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
